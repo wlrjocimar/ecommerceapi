2c61e3cd6198d7c9c60e8a0e0e57878860897278
Jocimar Walter <jocimarwalter@bb.com.br>
2025-10-26 19:09:49 -0300

    Initial commit

--- src/main/java/br/com/ecommerceapi/controllers/ProductController.java
+++ src/main/java/br/com/ecommerceapi/controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import br.com.ecommerceapi.services.ProductService;
 
 @RestController
 @RequestMapping("products")
+@CrossOrigin("*")
 public class ProductController {
 	
 	@Autowired
