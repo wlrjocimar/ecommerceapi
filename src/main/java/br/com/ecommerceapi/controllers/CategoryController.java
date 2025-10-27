487d8aba90b7f44d1a4ffa188deba4bdd4eb77dd
Jocimar Walter <jocimarwalter@bb.com.br>
2025-10-27 10:33:01 -0300

    atualizado CategoryController

--- src/main/java/br/com/ecommerceapi/controllers/CategoryController.java
+++ src/main/java/br/com/ecommerceapi/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
 	
-	
+	@GetMapping
 	public ResponseEntity<List<Category>> getCategories(){
 		
 		List<Category> categories = categoryService.getAllCategories();
