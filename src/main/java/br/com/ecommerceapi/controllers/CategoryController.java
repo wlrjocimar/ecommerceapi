02dfe514c33d3c67f830c3d912e99cb848021ced
Jocimar Walter <jocimarwalter@bb.com.br>
2025-10-27 20:26:48 -0300

    correção metodo newCategory

--- src/main/java/br/com/ecommerceapi/controllers/CategoryController.java
+++ src/main/java/br/com/ecommerceapi/controllers/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,7 @@ public class CategoryController {
 		
 	}
 
-	
+	@PostMapping
 	public ResponseEntity<Category> newGategory(@RequestBody Category category){
 		
 		Category savedCategory = categoryService.newCategory(category);
