cd56159e04df4273f4c1b0ee7951df79fce10b7c
Jocimar Walter <jocimarwalter@bb.com.br>
2025-10-29 11:29:13 -0300

    updated to fix ProductController bug

--- src/main/java/br/com/ecommerceapi/controllers/ProductController.java
+++ src/main/java/br/com/ecommerceapi/controllers/ProductController.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,5 +56,18 @@ public class ProductController {
 		return ResponseEntity.created(location).body(savedProduct);
 		
 	}
+	
+	
+	
+	@DeleteMapping
+	public ResponseEntity<HttpStatus> removeProduct(@PathVariable Integer productId){
+		
+		productService.removeProductById(productId);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	
 
 }
